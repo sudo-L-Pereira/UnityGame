bf4a70b3e582b60b59c4ffbf57a07a01e215af9d
sudo-L-Pereira <lpereira105@gmail.com>
2015-11-26 16:18:28 -0500

    Added an initial version of item collider
    item collider will only have coin, health and may be ammuntion colliders
    in it
    a seperate file will be made for damage

---
+++ code/item_collider.js
@@ -0,0 +1,30 @@
+﻿#pragma strict
+
+//solely item pick up colliders only
+
+import System.Collections.Generic;
+
+var coin:int = 0;                                                            //start from 0
+public var health:int = 100;                                                 //health starts full on begining game
+
+
+function OnTriggerEnter(other:Collider) {
+    if(other.tag =="hp_potion" && health < 100) {                           //hp_potion = tag on helth item
+        health++;                                                           //how much hp should the potion give ?
+        Destroy(other.gameObject);
+    }
+	
+    if(other.tag=="hp_potion" && health==100){
+        Destroy(other.gameObject);
+    }
+
+    if(other.tag =="coin" && coin < 100) {                                //coin = tag on coins
+        coin += 5;                                                          //takin that each coin gives 5 points 
+        Destroy(other.gameObject);
+    }
+	
+    if(other.tag=="coin" && coin==100){
+        Destroy(other.gameObject);
+    }
+	                                                                      //can add item collider for ammunition pick ups..
+}
